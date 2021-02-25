6acf3565e1b8d2e1505f09327682fbf1f52621bb
Taranjeet Singh <blumedata@gmail.com>
2021-02-25 16:59:23 +0530

    Updates

---
+++ precache-manifest.dc964b7aade6c2998ecda68aefb3d440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39755b962a3f9f82f6e2e37bc720b7b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6b360b4e80fa14ed0a37",
+    "url": "./static/css/main.06617b5f.chunk.css"
+  },
+  {
+    "revision": "8b6d65305dc9b9677747",
+    "url": "./static/js/2.5ee02913.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "./static/js/2.5ee02913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b360b4e80fa14ed0a37",
+    "url": "./static/js/main.e52846f0.chunk.js"
+  },
+  {
+    "revision": "a97b048c03829b284e9e",
+    "url": "./static/js/runtime-main.c39b3d03.js"
+  }
+]);
